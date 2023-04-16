5f822a76e910f9cc1af59311ec06debe48958c85
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-04-16 15:36:35 +0530

    add: home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './common/Navbar'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
