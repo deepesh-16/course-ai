7c1d86d535eb7886ecbeb46f147c31315dea335c
deepesh_16 <ks.deepesh16@gmail.com>
2023-04-16 20:06:38 +0530

    app.js

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 import pickle
 from pathlib import Path
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 root = Path("C:/Users/ksdee/Desktop/courseai/models")
 file=root/'data.pkl'
 fileobj=open(file,'rb')
@@ -15,4 +18,55 @@ import operator
 import itertools
 sort_by_val=operator.itemgetter(1)
 
-#Function that fetches jaccard function 
+#Function that fetches jaccard function
+# import operator
+import itertools
+sort_by_val=operator.itemgetter(1)
+def domain_tosearch(words):
+    dict_score={}
+    for i in range(len(domain)):
+        temp=jaccard(domain[i],words)
+        dict_score[i]=temp
+    dict_score=sorted(dict_score.items(),key=sort_by_val,reverse=True)
+    dict_score={k:v for k,v in dict_score}
+    dict_score=dict(itertools.islice(dict_score.items(), 10))
+    final_domain=[]
+    for key in dict_score:
+        final_domain.extend(domain[key])
+    set_res=set(final_domain)
+    final_domain=list(set_res)
+    final_domain=[i for i in final_domain if i not in words]
+    ans=""
+    for i in final_domain:
+        ans=ans+i+','
+    return ans
+
+#Reading Data
+df= pd.read_csv('C:/Users/ksdee/Desktop/courseai/data/data_with_tags.csv')
+ 
+#Recommendation Function
+def get_recommendation_tfidf_cosinSim(user_record,data=df):
+  df.loc[len(df)]=user_record
+  tfv= TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1,3),stop_words='english')
+  df['tags']=df['tags'].fillna('')
+  tfv_matrix=tfv.fit_transform(df['tags'])
+  cos_sim=cosine_similarity(tfv_matrix,tfv_matrix)
+  indices=pd.Series(df.index,index=df['Title']).drop_duplicates()
+  idx=indices[user_record['Title']]
+  # print('title',user_record['Title'])
+  # print('idx',idx)
+  cosSim_scores=list(enumerate(cos_sim[idx]))
+  cosSim_scores=sorted(cosSim_scores,key=lambda x:x[1],reverse=True)
+  cosSim_scores=cosSim_scores[1:16]
+  course_indices=[i[0] for i in cosSim_scores]
+  df_new=df.iloc[course_indices]
+  df_new=df_new.sort_values(by=['Stars'],ascending=False)
+  return df_new
+
+id=df.shape[0]+1
+title='user_rec'+str(id)
+domains=domain_tosearch(['r','python','data science'])
+user_record={'Sno':id,'Title':title,'Stars':'0','Link':'none','tags':domains}
+print(df.shape)
+recommendations=get_recommendation_tfidf_cosinSim(user_record)
+print(recommendations)
